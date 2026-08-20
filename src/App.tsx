34cd57dd03e3014947cfe9196797841bc8eb2cca
Brijendra Yadav <brijendrayadav2009@gmail.com>
2026-08-20 07:04:25 +0530

    Change API_BASE to local development URL

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { SystemSettings } from './components/pages/SystemSettings';
 import { DashboardView } from './components/pages/DashboardView';
 import { NotFound } from './components/pages/NotFound';
 
-const API_BASE = 'https://api.axionsystems.de';
+const API_BASE = 'http://localhost:9000';
 
 function App() {
   const location = useLocation();
